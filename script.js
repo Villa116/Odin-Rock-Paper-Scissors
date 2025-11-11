db23d2c6a254aa2a4a89f9de01a4bb43d5111fcd
Villa116 <vbmayombo@gmail.com>
2025-11-11 21:00:50 +0200

    complete logic to play a single round

--- script.js
+++ script.js
@@ -10,12 +10,41 @@ function getComputerChoice() {
     } else if (randomNum === 2) {
         computerChoice = "paper";
     } else {
-        computerChoice = "sissors";
+        computerChoice = "sicssors";
     }
     return computerChoice;
 }
 
 function getHummanChoice() {
-    let userChoice = prompt("Please enter your choice (rock, paper or sissors): ");
-    return userChoice;
+    let userChoice = prompt("Please enter your choice (rock, paper or scissors): ");
+    return userChoice.toLocaleLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It is a tie, both players chose ${humanChoice}`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`You win! human choice: ${humanChoice} crushes computer choice: ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log(`You lose! computer choice: ${computerChoice} crushes human choice: ${humanChoice}`);
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! human choice: ${humanChoice} cuts computer choice: ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log(`You lose! computer choice: ${computerChoice} cuts human choice: ${humanChoice}`);
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You win! human choice: ${humanChoice} covers computer choice: ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`You lose! computer choice: ${computerChoice} covers human choice: ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+let humanSelection = getHummanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
